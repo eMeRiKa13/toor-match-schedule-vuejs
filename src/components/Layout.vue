5808eb4bd2a36c3e9c7cea15fd85288fb9218e03
eMeRiKa <emerika13@gmail.com>
2021-08-01 10:53:55 +0200

    Working on header

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,73 +1,31 @@
 <template>
-  <div class="min-h-screen bg-gray-100">
-    <div class="max-w-3xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-    <div class="relative py-5 flex items-center justify-center lg:justify-between">
-        <!-- Logo -->
-        <div class="absolute left-0 flex-shrink-0 lg:static">
-        <a href="#">
-            <span class="sr-only">Workflow</span>
-            <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-300.svg" alt="Workflow" />
-        </a>
-        </div>
-
-        <!-- Right section on desktop -->
-        <div class="hidden lg:ml-4 lg:flex lg:items-center lg:pr-0.5">
-        <button type="button" class="flex-shrink-0 p-1 text-indigo-200 rounded-full hover:text-white hover:bg-white hover:bg-opacity-10 focus:outline-none focus:ring-2 focus:ring-white">
-            <span class="sr-only">View notifications</span>
-            <BellIcon class="h-6 w-6" aria-hidden="true" />
-        </button>
-    
-        </div>
-    </div>
-    </div>
-
-    <main class="-mt-24 pb-8">
-      <div class="max-w-3xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-        <h1 class="sr-only">Page title</h1>
-        <!-- Main 3 column grid -->
-        <div class="grid grid-cols-1 gap-4 items-start lg:grid-cols-3 lg:gap-8">
-          <!-- Left column -->
-          <div class="grid grid-cols-1 gap-4 lg:col-span-2">
-            <section aria-labelledby="section-1-title">
-              <h2 class="sr-only" id="section-1-title">Section title</h2>
-              <div class="rounded-lg bg-white overflow-hidden shadow">
-                <div class="p-6">
-                  <!-- Your content -->
-                </div>
-              </div>
-            </section>
+  <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+      <div class="relative flex items-center justify-between h-16">
+      
+        <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+          <div class="flex-shrink-0 flex items-center">
+            <img class="block lg:hidden h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow" />
+            <img class="hidden lg:block h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg" alt="Workflow" />
           </div>
-
-          <!-- Right column -->
-          <div class="grid grid-cols-1 gap-4">
-            <section aria-labelledby="section-2-title">
-              <h2 class="sr-only" id="section-2-title">Section title</h2>
-              <div class="rounded-lg bg-white overflow-hidden shadow">
-                <div class="p-6">
-                  <!-- Your content -->
-                </div>
-              </div>
-            </section>
+          <div class="hidden sm:block sm:ml-6">
+            <div class="flex space-x-4">
+              <a v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'px-3 py-2 rounded-md text-sm font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</a>
+            </div>
           </div>
         </div>
+        
       </div>
-    </main>
-    <footer>
-      <div class="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-7xl">
-        <div class="border-t border-gray-200 py-8 text-sm text-gray-500 text-center sm:text-left"><span class="block sm:inline">&copy; 2021 Tailwind Labs Inc.</span> <span class="block sm:inline">All rights reserved.</span></div>
-      </div>
-    </footer>
-  </div>
+    </div>
 </template>
 
 <script>
-import { BellIcon } from '@heroicons/vue/outline'
 
 export default {
   components: {
-    BellIcon,
+  
   },
   setup() {
+
     return {
     }
   },
